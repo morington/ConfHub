fd46709fbb950fdfeaf25c6f49b087a18494b52d
morington <morington.mail@gmail.com>
2024-04-17 00:25:34 +0300

    Added tests

---
+++ tests/test_confhub.py
@@ -0,0 +1,20 @@
+from confhub.models import URLConfig
+from confhub.reader import ReaderConf
+
+
+def test_model() -> None:
+    url_config = URLConfig(
+        scheme='http',
+        port=8080
+    )
+
+    assert url_config.scheme == 'http'
+    assert url_config.port == 8080
+
+
+def test_reader() -> None:
+    reader = ReaderConf('tests/settings.yml', 'tests/.secrets.yml', env='tests/.env', dev=True)
+    reader.create_service_urls()
+    configuration = reader.data
+
+    assert configuration.get('postgresql_url') == 'postgresql+asyncpg://morington:password@127.0.0.1:5432/test_database'
