19651b5f570f621c6c1f63d580089d747cda8a8c
Adam <123621952+morington@users.noreply.github.com>
2024-06-30 23:41:17 +0300

    Improved bugs

--- confhub/core/error.py
+++ confhub/core/error.py
@@ -6,7 +6,10 @@ logger: structlog.BoundLogger = structlog.getLogger("confhub")
 
 
 class ConfhubError(Exception):
-    ...
+    def __init__(self, message, **kwargs):
+        logger.error(message, exc_info=True, **kwargs)
+
+        super().__init__(message)
 
 
 def confhub_error(message: str, **kwargs) -> None:
